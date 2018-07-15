eec22c3d7a9ce110f9d3bb05d6116ba33781c814
Liaixue <894236735@qq.com>
2018-07-15 16:24:00 +0800

    Revert "read"
    This reverts commit 97795f30875ff5a777fe9c20b8d21a38f68a1af9.

--- kittenbook/js/prompt.js
+++
@@ -1,5 +0,0 @@
-/*alert('Hello, Li Aixue!');*/
-var userName = prompt('Hello, what\'s your name?');
-document.body.innerHTML = '<h1>Hello, ' + userName + '!</h1>' +
-						'<p>' + projectName + ' ' + versionNumber +
-						' accessed on: ' + currentTime + '</p>'; 
